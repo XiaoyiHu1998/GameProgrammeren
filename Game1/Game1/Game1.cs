3bb7f1dc720c07e1f78083fb3aefb827d202f8b7
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2019-09-25 16:57:30 +0200

    wat we in werkcollege 3 gedaan hebben.

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Game1
 {
+
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
@@ -11,7 +12,23 @@ namespace Game1
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D paddleRed;
+        Texture2D paddleBlue;
+        Texture2D ball;
 
+        Vector2 player1Location = new Vector2(50, 200);
+        Vector2 player2Location = new Vector2(735, 200);
+        Vector2 ballLocation = new Vector2(350, 225);
+
+        Vector2 ballVelocity = new Vector2(2, 0);
+        Vector2 ballAccelleration = new Vector2(0.35f, 0);
+
+        KeyboardState input = Keyboard.GetState();
+
+
+        float[] yAccelarationArrayPos = new float[] { 0.5f, 1.0f, 1.5f, 2.0f };
+        float[] yAccelarationArrayNeg = new float[]  { 0.5f, -1.0f, -1.5f, -2.0f };
+        
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -39,6 +56,10 @@ namespace Game1
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            paddleRed = Content.Load<Texture2D>("rodeSpeler");
+            paddleBlue = Content.Load<Texture2D>("blauweSpeler");
+            ball = Content.Load<Texture2D>("bal");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -52,6 +73,25 @@ namespace Game1
             // TODO: Unload any non ContentManager content here
         }
 
+        protected bool ballPaddleCollision()
+        {
+            if ((ballLocation.X >= 720 && ballLocation.X <= 730) && (ballLocation.Y <= (player2Location.Y + 100) && ballLocation.Y >= player2Location.Y))
+            {
+                return true;
+            }
+            else if ((ballLocation.X <= 60 && ballLocation.X >= 50) && (ballLocation.Y <= (player1Location.Y + 100) && ballLocation.Y >= player1Location.Y))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        protected void gameOver()
+        {
+
+        }
+
         /// <summary>
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
@@ -59,8 +99,82 @@ namespace Game1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //check input from keyboard
+            input = Keyboard.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || input.IsKeyDown(Keys.Escape))
                 Exit();
+
+            //Player1 movement
+            if (input.IsKeyDown(Keys.W) && player1Location.Y > 0)
+            {
+                player1Location.Y -= 6;
+            }
+            else if (input.IsKeyDown(Keys.S) && player1Location.Y < (GraphicsDevice.Viewport.Bounds.Height - 100))
+            {
+                player1Location.Y += 6;
+            }
+
+            //Player2 movement
+            if (input.IsKeyDown(Keys.Up) && player2Location.Y > 0)
+            {
+                player2Location.Y -= 6;
+            }
+            else if (input.IsKeyDown(Keys.Down) && player2Location.Y < (GraphicsDevice.Viewport.Bounds.Height - 100))
+            {
+                player2Location.Y += 6;
+            }
+
+            //ball movement
+
+            //check game over
+            if (ballLocation.X <= 0 || ballLocation.X >= GraphicsDevice.Viewport.Bounds.Width)
+            {
+                ballLocation.X = GraphicsDevice.Viewport.Bounds.Width / 2;
+                ballLocation.Y = GraphicsDevice.Viewport.Bounds.Height / 2;
+
+                ballVelocity = new Vector2(2, 0);
+            }
+
+            //check Y wall collisions
+            if (ballLocation.Y <= 0 || ballLocation.Y >= GraphicsDevice.Viewport.Bounds.Height)
+            {
+                ballVelocity.Y *= -1.0f;
+            }
+
+            //check playerColliisions and ball x direction
+            if (ballPaddleCollision())
+            {
+                ballVelocity.X *= -1.0f;
+
+                System.Random rand = new System.Random();
+                if (rand.Next(0, 2) == 1)
+                {
+                    ballVelocity.Y += yAccelarationArrayNeg[rand.Next(0, 4)];
+                }
+                else
+                {
+                    ballVelocity.Y += yAccelarationArrayPos[rand.Next(0, 4)];
+                }
+            }
+
+
+            //update ballvelocity
+            if(ballPaddleCollision() && ballVelocity.X < 3.3f)
+            {
+                if(ballVelocity.X > 0)
+                {
+                    ballVelocity += ballAccelleration;
+                }
+                else
+                {
+                    ballVelocity -= ballAccelleration;
+                }
+            }
+
+            //update location
+            ballLocation += ballVelocity;
+            
 
             // TODO: Add your update logic here
 
@@ -73,7 +187,12 @@ namespace Game1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Green);
+            spriteBatch.Begin();
+            spriteBatch.Draw(paddleRed, player1Location, Color.White);
+            spriteBatch.Draw(paddleBlue, player2Location, Color.White);
+            spriteBatch.Draw(ball, ballLocation, Color.White);
+            spriteBatch.End();
 
             // TODO: Add your drawing code here
 
