93611a3348f0b53b469b74ee4eb8bd0f9df9a22c
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2019-10-16 14:33:28 +0200

    added code for gamestate and points in GameWorld.cs.

--- Tetris/TetrisTemplate/GameWorld.cs
+++ Tetris/TetrisTemplate/GameWorld.cs
@@ -13,6 +13,7 @@ class GameWorld
     /// </summary>
     enum GameState
     {
+        StartScreen,
         Playing,
         GameOver
     }
@@ -38,10 +39,16 @@ class GameWorld
     /// </summary>
     TetrisGrid grid;
 
+    /// <summary>
+    /// The score of the player.
+    /// </summary>
+    int score;
+
     public GameWorld()
     {
         random = new Random();
         gameState = GameState.Playing;
+        score = 0;
 
         font = TetrisGame.ContentManager.Load<SpriteFont>("SpelFont");
 
@@ -59,9 +66,35 @@ class GameWorld
 
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
-        spriteBatch.Begin();
-        grid.Draw(gameTime, spriteBatch);
-        spriteBatch.End();
+        {
+            Vector2 textLocationStart = new Vector2(400, 200);
+            Vector2 textLocationScore = new Vector2(400, 200);
+            Vector2 textLocationVolgendBlock = new Vector2(400, 250);
+
+            switch (gameState)
+            {
+                case 0:
+                    spriteBatch.Begin();
+                    spriteBatch.DrawString(font, "Press space to start!", textLocationStart, Color.Black);
+                    spriteBatch.End();
+
+                    break;
+                case 1:
+                    spriteBatch.Begin();
+                    spriteBatch.DrawString(font, "Score van levens: " + score, textLocationScore, Color.Black);
+                    spriteBatch.DrawString(font, "Volgend blok:" + volgendBlok, textLocationVolgendBlock, Color.Black);
+                    grid.Draw(gameTime, spriteBatch);
+                    spriteBatch.End();
+                    break;
+                case 2:
+                    spriteBatch.Begin();
+                    spriteBatch.DrawString(font, "Game Over: press Enter to return to start!", textLocationEnd, Color.Black);
+                    spriteBatch.End();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     public void Reset()
